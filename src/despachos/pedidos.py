c6b872c9066c32e979dde5a45c7ee23fd0af2016
alulab14 <alulab14@inf.pucp.edu.pe>
2026-09-10 16:53:55 -0500

    se eliminaron las 2 lineas agregadas

--- src/despachos/pedidos.py
+++ src/despachos/pedidos.py
@@ -5,8 +5,7 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from datetime import datetime
 from enum import Enum
-#este es una linea ejemplo
-#segunda linea ejemplo
+
 
 class Estado(str, Enum):
     REGISTRADO = "registrado"
